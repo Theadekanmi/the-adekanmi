9817cfa32d5f8e884cb3e19daee2146a8c0974e7
Theadekanmi <theadekanmi@gmail.com>
2025-05-30 15:09:18 -0500

    new changes

---
+++ app/pages/500.js
@@ -0,0 +1,15 @@
+// /pages/500.js
+export default function Custom500() {
+  return (
+    <section className="w-full min-h-screen flex flex-col items-center justify-center text-center px-6 bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-white">
+      <h1 className="text-5xl font-bold text-red-600">500 - Internal Server Error</h1>
+      <p className="text-lg mt-4">
+        Oops! Something went wrong on our end. Please try refreshing the page or come back later.
+      </p>
+
+      <a href="/" className="bg-blue-600 text-white px-6 py-3 rounded-lg mt-6">
+        Go to Home
+      </a>
+    </section>
+  );
+}
